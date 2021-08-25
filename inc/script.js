7b01cb9c9f2ca7721d8ad115b5c8c7ea9ce5ce6b
Gyohdon <h.smith@live.nl>
2021-08-25 15:11:43 +0200

    Update script.js
    Removed setWrapper(id) as it was no longer necessary.
    Added calls to html element templates for rows, cards, images and titles.
    Updated displayData(dataObject) to make use of templates instead of hardcoding new HTML.

--- inc/script.js
+++ inc/script.js
@@ -10,6 +10,9 @@
  * 3. api_url: source for the dummy data
  * 4. wrapperId: name of the element id of the wrapper
  * 5. infinite_scroll_wrapper: the page element in which all data will be loaded
+ *
+ * templates:
+ * Rows, cards, images and titles will be loaded in by using templates from the HTML.
  * 
  * ON THE DUMMY DATA
  * the dummy data is data on various cryptocurrencies. the elements in each entry of the array are as follows:
@@ -19,15 +22,12 @@
  * [3] - acronym
  * [4] - logo
  * 
- * we will be displaying the name, acronym and logo of each coin in their element on the page
- * 
- * 
+ * we will be displaying the name and logo of each coin in their element on the page
  * 
  * functions:
  * 1. loadData(n): calls n amount of entries from API database
- * 2. displayData(rawData): puts data into visible elements into wrapper
+ * 2. displayData(dataObject): puts data into visible elements into wrapper by use of templates
  * 3. bottomIsVisible(pageElement): checks to see if pageElement's bottom is in pageview
- * 4. setWrapper(elementId): necessary for external linking of script
  * 
  */
 
@@ -35,6 +35,11 @@ const rowNum = 9;
 const colNum = 3;
 const api_url = "https://random-data-api.com/api/crypto_coin/random_crypto_coin";
 const wrapperId = "infinite-scroll-wrapper";
+
+const rowTemplate = document.getElementsByClassName("row")[0];
+const cardTemplate = rowTemplate.getElementsByClassName("card")[0];
+const imgTemplate = cardTemplate.getElementsByClassName("card__img")[0];
+const titleTemplate = cardTemplate.getElementsByClassName("card__title")[0];
 
 var infinite_scroll_wrapper = document.getElementById(wrapperId);
 
@@ -47,34 +52,24 @@ function loadData(n) {
 }
 
 function displayData(dataObject) {
-	var htmlOutput = '';
-	
 	for (var rows = 0; rows < rowNum; rows++) {
-		
-		htmlOutput += '<div id="row">';
+		var row = infinite_scroll_wrapper.appendChild(rowTemplate.cloneNode());
 		
 		for (var columns = 0; columns < colNum; columns++) {
 			var arrayIndex = (rows*colNum)+columns;
-			const entryObject = dataObject[arrayIndex];
+			const cardObject = dataObject[arrayIndex];
 			
-			htmlOutput += '<div id="column">';
-			htmlOutput += entryObject.coin_name + " : " + entryObject.acronym + "<br />";
-			htmlOutput += "<img src='" + entryObject.logo + "'>";
-			htmlOutput += '</div>';
+			var card = row.appendChild(cardTemplate.cloneNode());
+			var img = card.appendChild(imgTemplate.cloneNode());
+			var title = card.appendChild(titleTemplate.cloneNode());
+			img.src = cardObject.logo;
+			title.innerHTML = cardObject.coin_name;
 		}
-		
-		htmlOutput += '</div>';
 	}
-	
-	infinite_scroll_wrapper.innerHTML += htmlOutput;
 }
 
 function bottomIsVisible(pageElement) {
 	var elementDimensions = pageElement.getBoundingClientRect();
 	var elementBottom = elementDimensions.bottom;
 	return (window.innerHeight >= elementBottom && elementBottom > 0);
-}
-
-function setWrapper (elementId) {
-	infinite_scroll_wrapper = document.getElementById(elementId);
 }
